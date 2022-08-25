b3e585bc9d295306d4589e7136b080bcd287992d
José Marinho <clowdcap@hotmail.com>
2022-08-25 16:40:10 -0300

    upp mapbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 import './App.css'
 
+import Map, { Marker, Popup } from "react-map-gl";
+import { Room } from '@material-ui/icons'
+
 const App = () => {
+
+  const [showPopup, setShowPopup] = useState(true);
+
   return (
-    <div>App</div>
+    
+    <div className="App">
+      <Map 
+        initialViewState = {{
+          latitude: -25.3695,
+          longitude: -49.4512,
+          zoom: 14,
+        }}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX} 
+        mapStyle="mapbox://styles/mapbox/streets-v9">
+
+   
+        {showPopup && (
+          <Popup longitude={-25.3695} latitude={-49.4512}
+            anchor="bottom"
+            onClose={() => setShowPopup(false)}> You are here </Popup>)}
+      </Map>
+    </div>
   )
 }
 
