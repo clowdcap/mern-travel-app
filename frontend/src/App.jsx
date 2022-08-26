eda0fb90814294ce7acf8e7c27abd01abc4c9a0d
José Marinho <clowdcap@hotmail.com>
2022-08-26 14:45:48 -0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,128 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css'
 
 import Map, { Marker, Popup } from "react-map-gl";
-import { Room } from '@material-ui/icons'
+import { Room, Star } from '@material-ui/icons'
 
+import axios from "axios"
+// import { format } from 'timeago.js'
+// {format(p.createdAt)}
 const App = () => {
+  const currentUser = 'José Marinho'
+  const [pins, setPins] = useState([])
+  const [currentPlaceId, setCurrentPlaceId] = useState(null)
+  const [newPlace, setNewPlace] = useState(null)
+  const [viewport, setViewport] = useState({
+    width: "80vw",
+    height: "80vh",
+    latitude: -25.3695,
+    longitude: -49.4512,
+    zoom: 14,
+  })
 
-  const [showPopup, setShowPopup] = useState(true);
-
-  return (
+  useEffect(()=> {
     
-    <div className="App">
-      <Map 
-        initialViewState = {{
-          latitude: -25.3695,
-          longitude: -49.4512,
-          zoom: 14,
-        }}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX} 
-        mapStyle="mapbox://styles/mapbox/streets-v9">
+    const get_pins = async () => {
+      try {
+        const res = await axios.get("/pins")
+        setPins(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    get_pins()
+  }, [])
 
-   
-        {showPopup && (
-          <Popup longitude={-25.3695} latitude={-49.4512}
-            anchor="bottom"
-            onClose={() => setShowPopup(false)}> You are here </Popup>)}
-      </Map>
-    </div>
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id)
+  }
+
+  const handleAddClick = (event) => {
+    const lngLat = event.lngLat
+    const [lat, lng] = lngLat
+    setNewPlace({
+      lat:lat, lng:lng
+    })
+  }
+  return (
+    <>
+      <div className="App">
+        <>
+          <Map 
+            initialViewState = {{ ...viewport }}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX} 
+            mapStyle="mapbox://styles/bigboss56/cl7aikmeg003714p9zs3ez5bk"
+            attributionControl="true"
+            onDblClick= { handleAddClick }>
+
+            { pins.map(p=> (
+          
+              <>
+                <Marker 
+                  longitude={p.long} 
+                  latitude={p.lat} 
+                  offsetLeft={-20}
+                  offsetTop={-10}
+                  anchor="bottom">
+
+                    <Room 
+                      style={{ fontSize: "30px", 
+                          color: p.username === currentUser ? "slateblue" : "tomato", 
+                          cursor:"pointer"}} 
+                      onClick = {()=>{handleMarkerClick(p._id)}}
+                    />
+                    
+                </Marker>
+
+                {p._id === currentPlaceId && (
+                  <Popup
+                    longitude={p.long} 
+                    latitude={p.lat} 
+                    anchor="top"
+                    closeButton={true}
+                    closeOnClick={false}
+                    onClose = {() => {setCurrentPlaceId(null)}}>
+                    <div className="card">
+                      <label>Place</label>
+                        <h4 className="place">{p.title}</h4>
+                      <label>Review</label>
+                        <p className="review">{p.rating}</p>
+                      <label>Rating</label>
+                        <div className="rating">
+                          <Star className="star"/>
+                          <Star className="star"/>
+                          <Star className="star"/>
+                          <Star className="star"/>
+                          <Star className="star"/>
+                        </div>
+                      <label>Information</label>
+                        <span className="information">{p.desc}</span>
+                      <label>Created by:</label>
+                        <p className="username">{p.username}</p>
+                        <p className="data">{p.createdAt}</p>
+                    </div>
+                  </Popup>
+                )}
+              </>
+            ))}
+
+            { newPlace && (
+              <Popup
+                longitude={newPlace.lng} 
+                latitude={newPlace.lat} 
+                anchor="top"
+                closeButton={true}
+                closeOnClick={false}
+                onClose = {() => setCurrentPlaceId(null)}>
+              </Popup> 
+            )}
+
+          </Map>
+        </>
+      </div> 
+    </>
   )
 }
 
 export default App
+
